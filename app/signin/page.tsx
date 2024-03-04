bdf44cef65bce883783f02068fa21812cbd11e0f
Levi Voelz <levi@hatchmatter.com>
2024-03-04 12:00:19 -0800

    fix build err

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
