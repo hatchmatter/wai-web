d6b80a3a069064260319f5283151b3632ec1ab36
Levi Voelz <levi@hatchmatter.com>
2024-03-11 15:13:30 -0700

    disable crisp support

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect, useState, ReactNode } from "react";
-import { usePathname } from "next/navigation";
-import { Crisp } from "crisp-sdk-web";
+// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { ReactNode } from "react";
+// import { usePathname } from "next/navigation";
+// import { Crisp } from "crisp-sdk-web";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
@@ -11,54 +11,54 @@ import config from "@/config";
 
 // Crisp customer chat support:
 // This component is separated from ClientLayout because it needs to be wrapped with <SessionProvider> to use useSession() hook
-const CrispChat = (): null => {
-  const pathname = usePathname();
+// const CrispChat = (): null => {
+//   const pathname = usePathname();
 
-  const supabase = createClientComponentClient();
-  const [data, setData] = useState(null);
+//   const supabase = createClientComponentClient();
+//   const [data, setData] = useState(null);
 
-  // This is used to get the user data from Supabase Auth (if logged in) => user ID is used to identify users in Crisp
-  useEffect(() => {
-    const getUser = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
+//   // This is used to get the user data from Supabase Auth (if logged in) => user ID is used to identify users in Crisp
+//   useEffect(() => {
+//     const getUser = async () => {
+//       const {
+//         data: { session },
+//       } = await supabase.auth.getSession();
 
-      if (session) {
-        setData(session.user);
-      }
-    };
-    getUser();
-  }, [supabase.auth]);
+//       if (session) {
+//         setData(session.user);
+//       }
+//     };
+//     getUser();
+//   }, [supabase.auth]);
 
-  useEffect(() => {
-    if (config?.crisp?.id) {
-      // Set up Crisp
-      Crisp.configure(config.crisp.id);
+//   useEffect(() => {
+//     if (config?.crisp?.id) {
+//       // Set up Crisp
+//       Crisp.configure(config.crisp.id);
 
-      // (Optional) If onlyShowOnRoutes array is not empty in config.js file, Crisp will be hidden on the routes in the array.
-      // Use <AppButtonSupport> instead to show it (user clicks on the button to show Crisp—it cleans the UI)
-      if (
-        config.crisp.onlyShowOnRoutes &&
-        !config.crisp.onlyShowOnRoutes?.includes(pathname)
-      ) {
-        Crisp.chat.hide();
-        Crisp.chat.onChatClosed(() => {
-          Crisp.chat.hide();
-        });
-      }
-    }
-  }, [pathname]);
+//       // (Optional) If onlyShowOnRoutes array is not empty in config.js file, Crisp will be hidden on the routes in the array.
+//       // Use <AppButtonSupport> instead to show it (user clicks on the button to show Crisp—it cleans the UI)
+//       if (
+//         config.crisp.onlyShowOnRoutes &&
+//         !config.crisp.onlyShowOnRoutes?.includes(pathname)
+//       ) {
+//         Crisp.chat.hide();
+//         Crisp.chat.onChatClosed(() => {
+//           Crisp.chat.hide();
+//         });
+//       }
+//     }
+//   }, [pathname]);
 
-  // Add User Unique ID to Crisp to easily identify users when reaching support (optional)
-  useEffect(() => {
-    if (data?.user && config?.crisp?.id) {
-      Crisp.session.setData({ userId: data.user?.id });
-    }
-  }, [data]);
+//   // Add User Unique ID to Crisp to easily identify users when reaching support (optional)
+//   useEffect(() => {
+//     if (data?.user && config?.crisp?.id) {
+//       Crisp.session.setData({ userId: data.user?.id });
+//     }
+//   }, [data]);
 
-  return null;
-};
+//   return null;
+// };
 
 // All the client wrappers are here (they can't be in server components)
 // 1. NextTopLoader: Show a progress bar at the top when navigating between pages
@@ -77,7 +77,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
       {/* Show Success/Error messages anywhere from the app with toast() */}
       <Toaster
         toastOptions={{
-          duration: 3000,
+          duration: 4000,
         }}
       />
 
@@ -88,7 +88,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
       />
 
       {/* Set Crisp customer chat support */}
-      <CrispChat />
+      {/* <CrispChat /> */}
     </>
   );
 };
