8014c02421831b2b930bee58d0f93d463b06b8ef
levihatchmatter <levi@hatchmatter.com>
2024-04-18 12:12:34 -0700

    perform health check

--- libs/wai.ts
+++ libs/wai.ts
@@ -3,24 +3,39 @@ interface RegisterCallResponse {
   sampleRate?: number;
 }
 
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+if (process.env.NODE_ENV === "development") {
+  headers.append("ngrok-skip-browser-warning", "true");
+}
+
 export async function registerCall(
   agentId: string,
-  accessToken: string,
   timezone?: string,
   callerId?: string
 ): Promise<RegisterCallResponse> {
-  const headers = new Headers();
-
-  headers.append("Content-Type", "application/json");
-
   const response = await fetch(`/api/call/register`, {
     method: "POST",
     headers,
     body: JSON.stringify({
       agentId,
       timezone,
-      callerId
+      callerId,
     }),
+  });
+
+  if (!response.ok) {
+    console.error(`Error: ${response.status} - ${response.statusText}`);
+    return {};
+  }
+
+  return await response.json();
+}
+
+export async function healthCheck() {
+  const response = await fetch(process.env.NEXT_PUBLIC_WSS_URL + "/_health", {
+    headers,
   });
 
   if (!response.ok) {
