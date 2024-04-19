78198e8ef29477398ec5b86c37e2bf80f6f3a165
Jackson Nadler-Block <jnadlerblock@gmail.com>
2024-04-19 14:41:33 -0700

    Fixed code review

--- app/talk/page.tsx
+++ app/talk/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 import { createClient } from "@/libs/supabase-client";
 import { useGetUser } from "@/hooks";
 
@@ -9,11 +8,6 @@ import ButtonAccount from "@/components/ButtonAccount";
 import ButtonSelectPerson from "@/components/ButtonSelectPerson";
 import Wai from "@/components/Wai";
 
-//export const dynamic = "force-dynamic";
-
-// This is a private page: It's protected by the layout.js component which ensures the user is authenticated.
-// It's a server component which means you can fetch data (like the user profile) before the page is rendered.
-// See https://shipfa.st/docs/tutorials/private-page
 export default function Talk() {
   const [currentCallerId, setCurrentCallerId] = useState<string | null>(null);
   const supabase = createClient();
@@ -21,7 +15,6 @@ export default function Talk() {
 
   useEffect(() => {
     async function fetchData() {
-
       const { data: lastCall } = await supabase
         .from("calls")
         .select("*")
@@ -38,12 +31,17 @@ export default function Talk() {
       fetchData();
     }
   }, [user]);
-  
+
   return (
     <main className="flex flex-col">
       <section className="flex flex-col min-h-[calc(100dvh)] max-w-7xl w-full mx-auto">
         <header className="flex items-center justify-end p-4">
-          <ButtonSelectPerson lastCallerId={currentCallerId} onChange={(e) => {setCurrentCallerId(e.target.value);}} />
+          <ButtonSelectPerson
+            lastCallerId={currentCallerId}
+            onChange={(e) => {
+              setCurrentCallerId(e.target.value);
+            }}
+          />
           <ButtonAccount />
         </header>
 
