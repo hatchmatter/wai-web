8acafc66746a68c32a1e5e286a54beb1249497a8
Jackson Nadler-Block <jnadlerblock@gmail.com>
2024-05-14 16:57:52 -0700

    fix: adjusted to work with only having the one prompt option

--- supabase/migrations/20240510202422_add_prompt_options_column.sql
+++ supabase/migrations/20240510202422_add_prompt_options_column.sql
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 alter table public.settings add column prompt_options jsonb not null default '{ "Mythical Characters": true }'::jsonb;
+=======
+alter table public.settings add column prompt_options jsonb not null default '{ "Mythical Characters": true }'::jsonb;
+>>>>>>> b31ab7e (fix: adjusted to work with only having the one prompt option)
